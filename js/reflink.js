2b20342ea98160f4f822884466556a37eecaeabb
vasyltarasiuk-dot <vasyl.tarasiuk.ua@gmail.com>
2024-09-11 11:37:16 +0300

    Update reflink.js

--- js/reflink.js
+++ js/reflink.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $("a").click(function(){
-    $("#reflink*").attr("href", "https://partnervavadarv.com/?promo=77ab7c4c-6502-476a-a0e7-1a4fcb1b6b8e&target=register");
+    $("#reflink*").attr("href", "https://partnervavadarv.com/?promo=844b3a8d-3583-47aa-9d55-2c1825475-lv&target=register");
   });
 });
